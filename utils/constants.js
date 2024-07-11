0c87e43be3d965696b87f3ba940ea418a959b160
chaitanya02082000 <chaitanyagithub0208@gmail.com>
2024-07-11 18:01:57 +0530

    remeoved conssnatns from gitignore for deploying

---
+++ utils/constants.js
@@ -0,0 +1,4 @@
+export const api="https://api.nasa.gov/planetary/apod?api_key=KUKIc9kkRALjcuzMs3xLM6SYBo2hNUDu5UfpIAbe&count=5&thumbs=true"
+export const url="https://rss.nytimes.com/services/xml/rss/nyt/Space.xml"
+export const api10="https://api.nasa.gov/planetary/apod?api_key=KUKIc9kkRALjcuzMs3xLM6SYBo2hNUDu5UfpIAbe&count=15&thumbs=true"
+export const articlesURL=`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=astronomy+OR+space&begin_date=20240101&fq=document_type:("article")+AND+news_desk:("Science")+AND+section_name:("Science")&api-key=EPfOPddruAGshjTtNBG2fnKimtRAOnrw&page=`
